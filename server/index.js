1c4b8753b723adcad2662df3b403476318148b7c
Sanjeev6235 <sanjeevkumar231120@gmail.com>
2025-12-30 16:30:03 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ app.get("/", (req,res) => {
 
 app.use('/book',bookRouter)
 
-app.listen(8000, ()=>{
-    console.log('Port Listening on 8000');
+// app.listen(8000, ()=>{
+//     console.log('Port Listening on 8000');
 
-} )
+// } )
+
+module.exports = app;
